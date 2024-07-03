03ab5b8680fd198480cdd8d1578213a241cd35eb
Oren3606 <oren.dev@icloud.com>
2024-07-03 23:42:19 +0300

    Lay down tasks and general ideas

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter
+import threading
+
+# TODO:
+#   1. this program will run as a background process, and whenever a cursor is created, a new thread of it will be
+#      created and a small window above cursor OR status bar at the bottom of cursor's window will be created with
+#      suggestions.
+#   2. the program has to only access current + last sentence defined by a dot and a space ('. '). possibly more in the
+#      future, depending on how smart suggestions are.
+#   3. save terms which would otherwise be considered typos/incorrect. never save full sentences unless told explicitly.
+#   3. terms should be rated based on their use.
+#   4. possibly have some kind of way to save shortcut terms.
+#   5. separate sentences into parts? this will make things easier and make sense but its less modular.
+#   6. implement vectors? well see how complicated/hard that is.
+
+#  ...Talking and making TODOs is easy... Time to do stuff. Oh, well.
+
+if __name__ == "__main__":
+    ...
